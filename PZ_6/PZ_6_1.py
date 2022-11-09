d649d64bfcb2ebfdc5363b403cee65a6edb04e90
Derjoner <x12gox@gmail.com>
2022-11-09 14:39:19 +0300

    Начало моей работы

--- PZ_6/PZ_6_1.py
+++ PZ_6/PZ_6_1.py
@@ -1,10 +1,19 @@
-from random import random
 from random import randint
 i = 0
 Ds = []
-a = randint(1, 100)
-while i <= a:
-    Ds.append(random())
+a = randint(1, 10)
+while i < a:
+    Ds.append(randint(0, 100))
     i+=1
-print(a)
-print(Ds)
+
+for x in Ds:
+    if Ds.index(x) == a-1:
+        y = Ds[Ds.index(x) - 1]
+    else:
+        y = Ds[Ds.index(x) + 1]
+    if (x % 2 == 0) and (y % 2 ==0):
+        print(y)
+        break
+    elif (x % 2 == 0) and (y % 2 ==0):
+        print(y)
+        break
