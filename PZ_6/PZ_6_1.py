6f131b19a677c302cc118d4f56a9ec7a4986fa70
Maria099 <kurienkomaria13@gmail.com>
2022-11-12 18:45:10 +0300

    Начало моей работы

--- PZ_6/PZ_6_1.py
+++ PZ_6/PZ_6_1.py
@@ -5,15 +5,17 @@ a = randint(1, 10)
 while i < a:
     Ds.append(randint(0, 100))
     i+=1
-
+print(Ds)
 for x in Ds:
     if Ds.index(x) == a-1:
         y = Ds[Ds.index(x) - 1]
     else:
         y = Ds[Ds.index(x) + 1]
     if (x % 2 == 0) and (y % 2 ==0):
-        print(y)
+        print(Ds.index(y)+1)
         break
-    elif (x % 2 == 0) and (y % 2 ==0):
-        print(y)
+    elif (x % 2 != 0) and (y % 2 !=0):
+        print(Ds.index(y)+1)
         break
+else:
+    print(0)
