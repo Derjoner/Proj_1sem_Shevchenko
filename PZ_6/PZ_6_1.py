c27df8ec94f18a0e5941647e4e4e8bf2c4646318
Derjoner <105584785+Derjoner@users.noreply.github.com>
2022-11-15 21:27:11 +0300

    Практическая № 6. Шевченко ИС-23

--- PZ_6/PZ_6_1.py
+++ PZ_6/PZ_6_1.py
@@ -1,21 +1,31 @@
+# Вариант 25
+# Дан целочисленный список размера N.
+# Проверить, чередуются ли в нём чётные и нечётные числа.
+# Если чередуются, то вывести 0, если нет, то вывести порядковый номер первого элемента,
+# нарущающего закономерность.
+
 from random import randint
+
 i = 0
-Ds = []
-a = randint(1, 10)
-while i < a:
-    Ds.append(randint(0, 100))
-    i+=1
-print(Ds)
-for x in Ds:
-    if Ds.index(x) == a-1:
-        y = Ds[Ds.index(x) - 1]
+N = randint(1, 10)
+A = []
+
+while i < N:
+    A.append(randint(0, 100))
+    i += 1
+
+print("Данный список:", A)
+
+for x in A:
+    if A.index(x) == N-1:
+        y = A[A.index(x) - 1]
     else:
-        y = Ds[Ds.index(x) + 1]
-    if (x % 2 == 0) and (y % 2 ==0):
-        print(Ds.index(y)+1)
+        y = A[A.index(x) + 1]
+    if (x % 2 == 0) and (y % 2 == 0):
+        print("Порядковый номер числа:", A.index(y) + 1)
         break
-    elif (x % 2 != 0) and (y % 2 !=0):
-        print(Ds.index(y)+1)
+    elif (x % 2 != 0) and (y % 2 != 0):
+        print("Порядковый номер числа:", A.index(y) + 1)
         break
 else:
     print(0)
