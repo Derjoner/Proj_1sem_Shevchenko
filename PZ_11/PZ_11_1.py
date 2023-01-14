8383f0171185a89333d29d2680af65dde929c638
Derjoner <x12gox@gmail.com>
2023-01-14 16:28:49 +0300

    Практическая №11. Шевченко ИС-23

--- PZ_11/PZ_11_1.py
+++ PZ_11/PZ_11_1.py
@@ -4,6 +4,7 @@ i = 0
 N = randint(3, 10)
 A = []
 B = []
+C = []
 
 while i < N:
     A.append(str(randint(0, 50)))
@@ -15,6 +16,9 @@ while i < N:
     B.append(str(randint(-50, -1)))
     i += 1
 
+C = A + B
+print(C.sort())
+
 f1 = open('new_plus.txt', 'w')
 for x in A:
     f1.writelines(x)
@@ -22,6 +26,7 @@ for x in A:
 f1.close()
 f1 = open('new_plus.txt')
 print(f1.read())
+f1.close()
 
 f2 = open('new_minus.txt', 'w')
 for x in B:
@@ -30,3 +35,14 @@ for x in B:
 f2.close()
 f2 = open('new_minus.txt')
 print(f2.read())
+f2.close()
+
+f3 = open('new_sort.txt', 'w')
+f3.write("Элементы первого и второго файлов: ")
+for i in open('new_plus.txt', encoding='UTF-8'):
+    f3.write(i)
+for i in open('new_minus.txt', encoding='UTF-8'):
+    f3.write(i)
+f3.write("\n")
+f3.write("Элементы после сортировки: ")
+f3.writelines()
