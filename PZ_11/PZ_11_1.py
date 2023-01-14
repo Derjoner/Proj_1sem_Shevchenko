328e5b530345525a07629d23050bcccec22e3986
Derjoner <x12gox@gmail.com>
2023-01-14 16:08:38 +0300

    Практическая №11. Шевченко ИС-23

---
+++ PZ_11/PZ_11_1.py
@@ -0,0 +1,32 @@
+from random import randint
+
+i = 0
+N = randint(3, 10)
+A = []
+B = []
+
+while i < N:
+    A.append(str(randint(0, 50)))
+    i += 1
+else:
+    i = 0
+
+while i < N:
+    B.append(str(randint(-50, -1)))
+    i += 1
+
+f1 = open('new_plus.txt', 'w')
+for x in A:
+    f1.writelines(x)
+    f1.write(" ")
+f1.close()
+f1 = open('new_plus.txt')
+print(f1.read())
+
+f2 = open('new_minus.txt', 'w')
+for x in B:
+    f2.writelines(x)
+    f2.write(" ")
+f2.close()
+f2 = open('new_minus.txt')
+print(f2.read())
