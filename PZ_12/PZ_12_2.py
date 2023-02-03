d999ad9469644ba72d77c0d588167071a963d700
Игорь Шевченко <105584785+Derjoner@users.noreply.github.com>
2023-02-03 09:22:48 +0300

    Практическая работа № 12. Шевченко Ис-23

---
+++ PZ_12/PZ_12_2.py
@@ -0,0 +1,13 @@
+# Вариант 25
+# Составить генератор (yield), который выводит из строки только цифры.
+
+from string import digits
+
+i = 0
+A = "A, f, 3, t, 5, j, 5, 2, F, A, 0"
+B = []
+
+def abc(srt):
+  yield from [x for x in srt if x in digits]
+
+print(list(abc(A)))
