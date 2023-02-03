353662aad23b517b9c98951cc2381fd219e12ca9
Игорь Шевченко <105584785+Derjoner@users.noreply.github.com>
2023-02-03 08:55:42 +0300

    Практическая работа № 12. Шевченко ИС-23

--- PZ_12/PZ_12_1.py
+++ PZ_12/PZ_12_1.py
@@ -1,11 +1,28 @@
+# Вариант 25
+#  Дана последовательность целых чисел.
+# Поменять местами её первую и последнюю трети.
+
 from random import randint
 
 i = 0
-N = randint(6, 45)
 A = []
+B = []
+C = []
 
-while i < N:
-    A.append(randint(1, 25))
-    i += 1
+while i < 9:
+  A.append(randint(1, 25))
+  i += 1
 
+def gener(lst, n):
+    for i in range(0, len(lst), n):
+        yield lst[i : i + n]
 
+B = list(gener(A, 3))
+
+B[0], B[-1] = B[-1], B[0]
+
+for x in B:
+  for y in x:
+    C.append(y)
+
+print(A, C)
