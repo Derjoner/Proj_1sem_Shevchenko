6d63e1646ed6a757ee7b08d97ce4099fd333dcee
Игорь Шевченко <105584785+Derjoner@users.noreply.github.com>
2023-02-10 21:56:23 +0300

    Практическая работа №13

--- PZ_13/PZ_13_2.py
+++ PZ_13/PZ_13_2.py
@@ -1,13 +1,17 @@
+# Вариант 25
+# В матрице найти сумму элементов второй половины матрицы
+
 import numpy as np
 
 A = np.random.randint(0, 20, (4, 4))
+
+print("Данная матрица:\n", A)
 
 def gener(lis):
     yield from [n** 2 for n in lis]
 
 C = list(gener(list(A[:, 1])))
 
+A[:, 1] = C[:]
 
-
-print(C)
-print(A, "\nfdf")
+print("Полученная матрица:\n", A)
