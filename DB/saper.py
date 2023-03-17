cb7ecded4b59e23ea44d292a3eb9b1ab17eb755c
Игорь Шевченко <105584785+Derjoner@users.noreply.github.com>
2023-03-17 18:04:50 +0300

    БД. Шевченко

--- DB/saper.py
+++ DB/saper.py
@@ -44,3 +44,20 @@ with sq.connect('saper.db') as con:
     cur.execute("SELECT * FROM users WHERE old BETWEEN 18 AND 20")
     result= cur.fetchall()
 print(result)
+
+
+with sq.connect('saper.db') as con:
+    cur = con.cursor()
+    cur.execute("UPDATE users SET old = 20 WHERE old = 19")
+
+with sq.connect('saper.db') as con:
+    cur = con.cursor()
+    cur.execute("UPDATE users SET score=score+300 WHERE score < 1000")
+
+with sq.connect('saper.db') as con:
+    cur = con.cursor()
+    cur.execute("UPDATE users SET score=score+100 WHERE old=20")
+
+with sq.connect('saper.db') as con:
+    cur = con.cursor()
+    cur.execute("UPDATE users SET score=score+1000 WHERE name LIKE 'Бася'")
