5c157b31751665d3ebdb0ab149db9b1ea17599df
Игорь Шевченко <105584785+Derjoner@users.noreply.github.com>
2023-03-17 17:36:50 +0300

    Работа по БД. Шевченко

--- DB/saper.py
+++ DB/saper.py
@@ -12,4 +12,35 @@ with sq.connect('saper.db') as con:
     #cur.execute("INSERT INTO users VALUES (1, 'Один', 1, 43, 59494)")
     #cur.execute("INSERT INTO users VALUES (2, 'Версаче', 1, 61, 5554)")
     #cur.execute("INSERT INTO users VALUES (3, 'Кравильда', 2, 15, 765)")
-    cur.executemany("INSERT INTO users VALUES (?, ?, ?, ?, ?)", info_users)
+    #cur.executemany("INSERT INTO users VALUES (?, ?, ?, ?, ?)", info_users)
+
+
+with sq.connect('saper.db') as con:
+    cur = con.cursor()
+    cur.execute("SELECT * FROM users")
+    result= cur.fetchall()
+print(result)
+
+with sq.connect('saper.db') as con:
+    cur = con.cursor()
+    cur.execute("SELECT * FROM users WHERE sex = 2")
+    result= cur.fetchall()
+print(result)
+
+with sq.connect('saper.db') as con:
+    cur = con.cursor()
+    cur.execute("SELECT * FROM users WHERE score<1000")
+    result= cur.fetchall()
+print(result)
+
+with sq.connect('saper.db') as con:
+    cur = con.cursor()
+    cur.execute("SELECT * FROM users WHERE score=(SELECT MAX(score) FROM users)")
+    result= cur.fetchall()
+print(result)
+
+with sq.connect('saper.db') as con:
+    cur = con.cursor()
+    cur.execute("SELECT * FROM users WHERE old BETWEEN 18 AND 20")
+    result= cur.fetchall()
+print(result)
